1f554ba50eb43e8c747539dea23d94e5281b1823
Kyouma <55413054+leonaurdo@users.noreply.github.com>
2024-08-21 21:53:25 -0400

    message sending implemented

--- client/client/src/components/App.js
+++ client/client/src/components/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import Login from './Login';
 import useLocalStorage from '../hooks/useLocalStorage';
 import Dashboard from './Dashboard';
+import { ContactsProvider } from '../contexts/ContactsProvider';
+import { ConversationsProvider } from '../contexts/ConversationsProvider';
 
 function App () {
   const [id, setId] = useLocalStorage()
 
+  const dashboard = (
+    <ContactsProvider>
+      <ConversationsProvider id={id}>
+      <Dashboard id={id} />
+      </ConversationsProvider>
+    </ContactsProvider>
+  )
+
   return (
-      id ? <Dashboard id={id} /> : <Login onIdSubmit={setId}/>
+      id ? dashboard : <Login onIdSubmit={setId}/>
   )
 }
 
